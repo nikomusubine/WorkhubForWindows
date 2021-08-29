7a68d8c83eb1cbfbd0b5665c86c02bd0c0644481
Niko Musubine <83539803+Nikochan2525@users.noreply.github.com>
2021-08-29 19:08:02 +0900

    Create Mainwindow.Designer.cs

---
+++ Mainwindow.Designer.cs
@@ -0,0 +1,50 @@
+﻿
+namespace WorkhubForWindows
+{
+    partial class Mainwindow
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Mainwindow));
+            this.SuspendLayout();
+            // 
+            // Mainwindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 389);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Mainwindow";
+            this.Text = "WorkHub";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
