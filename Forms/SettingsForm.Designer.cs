b00362b7de228914068da5247be50e936722d200
Niko Musubine <83539803+Nikochan2525@users.noreply.github.com>
2021-10-06 13:44:59 +0900

    Wanna add start up

--- Forms/SettingsForm.Designer.cs
+++ Forms/SettingsForm.Designer.cs
@@ -51,206 +51,145 @@
             this.OpacityBar = new System.Windows.Forms.TrackBar();
             this.OpacityLabel = new System.Windows.Forms.Label();
             this.OpacityBox = new System.Windows.Forms.NumericUpDown();
+            this.StartUpbutton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.OpacityBar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.OpacityBox)).BeginInit();
             this.SuspendLayout();
             // 
             // FontName
             // 
-            this.FontName.AutoSize = true;
-            this.FontName.Location = new System.Drawing.Point(17, 16);
-            this.FontName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            resources.ApplyResources(this.FontName, "FontName");
             this.FontName.Name = "FontName";
-            this.FontName.Size = new System.Drawing.Size(39, 15);
-            this.FontName.TabIndex = 0;
-            this.FontName.Text = "Font:";
             // 
             // FontNames
             // 
             this.FontNames.FormattingEnabled = true;
-            this.FontNames.Location = new System.Drawing.Point(92, 12);
-            this.FontNames.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.FontNames, "FontNames");
             this.FontNames.Name = "FontNames";
-            this.FontNames.Size = new System.Drawing.Size(160, 23);
-            this.FontNames.TabIndex = 1;
             // 
             // ApplyButton
             // 
-            this.ApplyButton.Location = new System.Drawing.Point(684, 406);
-            this.ApplyButton.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.ApplyButton, "ApplyButton");
             this.ApplyButton.Name = "ApplyButton";
-            this.ApplyButton.Size = new System.Drawing.Size(100, 31);
-            this.ApplyButton.TabIndex = 3;
-            this.ApplyButton.Text = "Apply";
             this.ApplyButton.UseVisualStyleBackColor = true;
             this.ApplyButton.Click += new System.EventHandler(this.ApplyClicked);
             // 
             // Cancel_Button
             // 
-            this.Cancel_Button.Location = new System.Drawing.Point(576, 406);
-            this.Cancel_Button.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.Cancel_Button, "Cancel_Button");
             this.Cancel_Button.Name = "Cancel_Button";
-            this.Cancel_Button.Size = new System.Drawing.Size(100, 31);
-            this.Cancel_Button.TabIndex = 4;
-            this.Cancel_Button.Text = "Cancel";
             this.Cancel_Button.UseVisualStyleBackColor = true;
             this.Cancel_Button.Click += new System.EventHandler(this.CancelClicked);
             // 
             // FontSizeBox
             // 
-            this.FontSizeBox.Location = new System.Drawing.Point(261, 14);
-            this.FontSizeBox.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.FontSizeBox, "FontSizeBox");
             this.FontSizeBox.Name = "FontSizeBox";
-            this.FontSizeBox.Size = new System.Drawing.Size(67, 22);
-            this.FontSizeBox.TabIndex = 5;
             this.FontSizeBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.FontSizeKeyPress);
             // 
             // backimglabel
             // 
-            this.backimglabel.AutoSize = true;
-            this.backimglabel.Location = new System.Drawing.Point(16, 46);
+            resources.ApplyResources(this.backimglabel, "backimglabel");
             this.backimglabel.Name = "backimglabel";
-            this.backimglabel.Size = new System.Drawing.Size(208, 15);
-            this.backimglabel.TabIndex = 6;
-            this.backimglabel.Text = "MainWindow BackGround Image";
             // 
             // backimgpath
             // 
-            this.backimgpath.Location = new System.Drawing.Point(230, 42);
+            resources.ApplyResources(this.backimgpath, "backimgpath");
             this.backimgpath.Name = "backimgpath";
-            this.backimgpath.Size = new System.Drawing.Size(174, 22);
-            this.backimgpath.TabIndex = 7;
             // 
             // backimgref
             // 
-            this.backimgref.Location = new System.Drawing.Point(410, 41);
+            resources.ApplyResources(this.backimgref, "backimgref");
             this.backimgref.Name = "backimgref";
-            this.backimgref.Size = new System.Drawing.Size(25, 25);
-            this.backimgref.TabIndex = 8;
-            this.backimgref.Text = "...";
             this.backimgref.UseVisualStyleBackColor = true;
             this.backimgref.Click += new System.EventHandler(this.BackImgRefClick);
             // 
             // widgetbackimgref
             // 
-            this.widgetbackimgref.Location = new System.Drawing.Point(410, 73);
+            resources.ApplyResources(this.widgetbackimgref, "widgetbackimgref");
             this.widgetbackimgref.Name = "widgetbackimgref";
-            this.widgetbackimgref.Size = new System.Drawing.Size(25, 25);
-            this.widgetbackimgref.TabIndex = 11;
-            this.widgetbackimgref.Text = "...";
             this.widgetbackimgref.UseVisualStyleBackColor = true;
             this.widgetbackimgref.Click += new System.EventHandler(this.WidgetBackImgRefClick);
             // 
             // widgetbackimgpath
             // 
-            this.widgetbackimgpath.Location = new System.Drawing.Point(230, 74);
+            resources.ApplyResources(this.widgetbackimgpath, "widgetbackimgpath");
             this.widgetbackimgpath.Name = "widgetbackimgpath";
-            this.widgetbackimgpath.Size = new System.Drawing.Size(174, 22);
-            this.widgetbackimgpath.TabIndex = 10;
             // 
             // widgetbackimg
             // 
-            this.widgetbackimg.AutoSize = true;
-            this.widgetbackimg.Location = new System.Drawing.Point(16, 74);
+            resources.ApplyResources(this.widgetbackimg, "widgetbackimg");
             this.widgetbackimg.Name = "widgetbackimg";
-            this.widgetbackimg.Size = new System.Drawing.Size(173, 15);
-            this.widgetbackimg.TabIndex = 9;
-            this.widgetbackimg.Text = "Widget BackGround Image";
             // 
             // LogoffSoundbutton
             // 
-            this.LogoffSoundbutton.Location = new System.Drawing.Point(410, 133);
+            resources.ApplyResources(this.LogoffSoundbutton, "LogoffSoundbutton");
             this.LogoffSoundbutton.Name = "LogoffSoundbutton";
-            this.LogoffSoundbutton.Size = new System.Drawing.Size(25, 25);
-            this.LogoffSoundbutton.TabIndex = 17;
-            this.LogoffSoundbutton.Text = "...";
             this.LogoffSoundbutton.UseVisualStyleBackColor = true;
             this.LogoffSoundbutton.Click += new System.EventHandler(this.LogoffsoundrefClick);
             // 
             // LogoffsoundBox
             // 
-            this.LogoffsoundBox.Location = new System.Drawing.Point(230, 134);
+            resources.ApplyResources(this.LogoffsoundBox, "LogoffsoundBox");
             this.LogoffsoundBox.Name = "LogoffsoundBox";
-            this.LogoffsoundBox.Size = new System.Drawing.Size(174, 22);
-            this.LogoffsoundBox.TabIndex = 16;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 134);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 15);
-            this.label1.TabIndex = 15;
-            this.label1.Text = "LogoffSound";
             // 
             // Shutdownsoundbutton
             // 
-            this.Shutdownsoundbutton.Location = new System.Drawing.Point(410, 101);
+            resources.ApplyResources(this.Shutdownsoundbutton, "Shutdownsoundbutton");
             this.Shutdownsoundbutton.Name = "Shutdownsoundbutton";
-            this.Shutdownsoundbutton.Size = new System.Drawing.Size(25, 25);
-            this.Shutdownsoundbutton.TabIndex = 14;
-            this.Shutdownsoundbutton.Text = "...";
             this.Shutdownsoundbutton.UseVisualStyleBackColor = true;
             this.Shutdownsoundbutton.Click += new System.EventHandler(this.ShutdownrefClick);
             // 
             // Shutdownsoundbox
             // 
-            this.Shutdownsoundbox.Location = new System.Drawing.Point(230, 102);
+            resources.ApplyResources(this.Shutdownsoundbox, "Shutdownsoundbox");
             this.Shutdownsoundbox.Name = "Shutdownsoundbox";
-            this.Shutdownsoundbox.Size = new System.Drawing.Size(174, 22);
-            this.Shutdownsoundbox.TabIndex = 13;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 106);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(115, 15);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "Shutdown Sound";
             // 
             // WidgetsSettingsLabel
             // 
-            this.WidgetsSettingsLabel.AutoSize = true;
-            this.WidgetsSettingsLabel.Font = new System.Drawing.Font("MS UI Gothic", 10F);
-            this.WidgetsSettingsLabel.Location = new System.Drawing.Point(13, 160);
-            this.WidgetsSettingsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            resources.ApplyResources(this.WidgetsSettingsLabel, "WidgetsSettingsLabel");
             this.WidgetsSettingsLabel.Name = "WidgetsSettingsLabel";
-            this.WidgetsSettingsLabel.Size = new System.Drawing.Size(116, 17);
-            this.WidgetsSettingsLabel.TabIndex = 18;
-            this.WidgetsSettingsLabel.Text = "WidgetSettings";
             // 
             // OpacityBar
             // 
-            this.OpacityBar.Location = new System.Drawing.Point(230, 188);
+            resources.ApplyResources(this.OpacityBar, "OpacityBar");
             this.OpacityBar.Maximum = 100;
             this.OpacityBar.Name = "OpacityBar";
-            this.OpacityBar.Size = new System.Drawing.Size(174, 56);
-            this.OpacityBar.TabIndex = 19;
             this.OpacityBar.TickStyle = System.Windows.Forms.TickStyle.None;
             // 
             // OpacityLabel
             // 
-            this.OpacityLabel.AutoSize = true;
-            this.OpacityLabel.Location = new System.Drawing.Point(17, 188);
+            resources.ApplyResources(this.OpacityLabel, "OpacityLabel");
             this.OpacityLabel.Name = "OpacityLabel";
-            this.OpacityLabel.Size = new System.Drawing.Size(55, 15);
-            this.OpacityLabel.TabIndex = 20;
-            this.OpacityLabel.Text = "Opacity";
             // 
             // OpacityBox
             // 
-            this.OpacityBox.Location = new System.Drawing.Point(92, 188);
+            resources.ApplyResources(this.OpacityBox, "OpacityBox");
             this.OpacityBox.Name = "OpacityBox";
-            this.OpacityBox.Size = new System.Drawing.Size(132, 22);
-            this.OpacityBox.TabIndex = 21;
+            // 
+            // StartUpbutton
+            // 
+            resources.ApplyResources(this.StartUpbutton, "StartUpbutton");
+            this.StartUpbutton.Name = "StartUpbutton";
+            this.StartUpbutton.UseVisualStyleBackColor = true;
             // 
             // SettingsForm
             // 
             this.AcceptButton = this.ApplyButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AllowDrop = true;
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StartUpbutton);
             this.Controls.Add(this.OpacityBox);
             this.Controls.Add(this.OpacityLabel);
             this.Controls.Add(this.OpacityBar);
@@ -272,10 +211,7 @@
             this.Controls.Add(this.ApplyButton);
             this.Controls.Add(this.FontNames);
             this.Controls.Add(this.FontName);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "SettingsForm";
-            this.Text = "Settings";
             this.Click += new System.EventHandler(this.ApplyClicked);
             ((System.ComponentModel.ISupportInitialize)(this.OpacityBar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.OpacityBox)).EndInit();
@@ -308,5 +244,6 @@
         private System.Windows.Forms.TrackBar OpacityBar;
         private System.Windows.Forms.Label OpacityLabel;
         private System.Windows.Forms.NumericUpDown OpacityBox;
+        private System.Windows.Forms.Button StartUpbutton;
     }
 }
--- Forms/SettingsForm.cs
+++ Forms/SettingsForm.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +14,13 @@ namespace WorkhubForWindows.Forms
 {
     public partial class SettingsForm : Form
     {
+        private string aplTitle = null; // アプリ名
+        private string exeFile = null;  // exeファイル名
+        private string jsFile = null;   // スクリプトファイル名
+        private string lnkFile = null;  // リンク名
+        private bool frmLoadFlg = false; // Form1ロード済みか
+        private bool isstartup = false;
+
         public SettingsForm()
         {
             InitializeComponent();
@@ -26,6 +35,38 @@ namespace WorkhubForWindows.Forms
             }
             initalizeform();
         }
+
+        private void Settings_Load(object sender, EventArgs e)
+        {
+            // プロジェクト＞プロパティ＞アセンブリ情報　で指定した「タイトル」を取得
+            var assembly = Assembly.GetExecutingAssembly();
+            var attribute = Attribute.GetCustomAttribute(
+              assembly,
+              typeof(AssemblyTitleAttribute)
+            ) as AssemblyTitleAttribute;
+            aplTitle = attribute.Title;
+            this.Text = aplTitle;
+
+            // 自身のexeファイル名を取得
+            exeFile = Path.GetFileName(Application.ExecutablePath);
+
+            // WSHスクリプト名
+            jsFile = Directory.GetParent(Application.ExecutablePath) + "\\addStartup.js";
+
+            // ショートカットのリンク名
+            String sMnu = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            lnkFile = sMnu + "\\" + aplTitle + ".lnk";
+
+
+            // スタートアップ有無
+            isstartup = File.Exists(lnkFile);
+
+            if (isstartup)
+            {
+
+            }
+        }
+
 
         private void FontSizeKeyPress(object sender, KeyPressEventArgs e)
         {
@@ -85,6 +126,11 @@ namespace WorkhubForWindows.Forms
         }
 
         private void OpacityChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddStartUpClicked(object sender,EventArgs e)
         {
 
         }
